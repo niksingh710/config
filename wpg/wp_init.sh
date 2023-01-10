420b526b767eb483917b6d71b4fc448a9463b1a5
nik.singh710@gmail.com <nik.singh710@gmail.com>
2023-01-10 20:00:58 +0530

    Added oomix theme

--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/niksingh710/Pictures/mywalls//tacosauceninja-remembering.jpg' '/home/niksingh710/Pictures/mywalls//tacosauceninja-remembering.jpg'
+wpg -nrs '/home/niksingh710/Pictures/mywalls//night-snowing.jpg' '/home/niksingh710/Pictures/mywalls//night-snowing.jpg'
